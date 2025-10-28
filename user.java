cbbe173d256e0f3f2f70e9392d463ec612a29590
backia05 <165563439+backia05@users.noreply.github.com>
2025-10-28 10:44:26 +0530

    Add files via upload

---
+++ user.java
@@ -0,0 +1,22 @@
+public class user{
+    public static void main(String[]args){
+        System.out.println("========================");
+        System.out.println("UserFinder Application");
+        String username="backia05";
+        System.out.println("===========================");
+        System.out.println("Username:backia05");
+        //Scanner sc=new Scanner (System.in);
+        //int id=sc.nextint();
+        //String user=sc.nextint();
+        //String Password=sc.next();
+        //String project=sc.next();
+        System.out.println("User Exists");
+        System.out.println("Details of the User in Github");
+        System.out.println("Name:backia05");
+        System.out.println("Projects:");
+        System.out.println("1.DocQure");
+        System.out.println("2.Digital Twin");
+        System.out.println("=============================");
+
+    }
+}
